242916aa62677caeecf095b6219a73599d051e00
Sepehr Vahedi <sepehr.vahedi@gmail.com>
2025-03-02 19:35:33 +0330

    Fix admin and local date conversion.

--- src/main/kotlin/army/booker/application/http/v1/bundle/BundleApplicationValueObjects.kt
+++ src/main/kotlin/army/booker/application/http/v1/bundle/BundleApplicationValueObjects.kt
@@ -3,11 +3,9 @@ package army.booker.application.http.v1.bundle
 import army.booker.domain.bundle.DailyReservation
 import army.booker.domain.bundle.ProductSorting
 import army.booker.domain.bundle.ProductType
-import jakarta.validation.constraints.Future
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
-import java.time.LocalDate
 
 data class CreateBundleRequest(
   @field:NotBlank
@@ -82,7 +80,6 @@ data class ReserveBundleRequest(
   val bundleId: String,
 
   @field:NotNull(message = "Reservation date is required")
-  @field:Future(message = "Reservation date must be in the future")
-  val reservationDate: LocalDate
+  val reservationDate: String,
 )
 
